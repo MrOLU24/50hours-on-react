30a1ac72f5c07fd6da1c4a0247d4a2803aedd437
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-03-13 22:50:04 +0100

    day 4 bro

--- index.jsx
+++ index.jsx
@@ -1,4 +1,7 @@
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<div>Day 3 bro</div>);
+root.render(<div> 
+
+    <h1>day 4 , have not learn anything</h1>
+</div>);
