72098e9c4227f0abb0b4cfeb6406e0b42db4aa98
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-03-14 23:01:31 +0100

    tailwind add

--- index.jsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<div> 
-
-    <h1>day 4 , have not learn anything</h1>
-</div>);
