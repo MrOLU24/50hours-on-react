9a78ebdeea0ed758a7ac93b92aca3a6e3f03ecab
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-03-24 21:41:12 +0100

    k

---
+++ script.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// A simple React component example
+
+
+function App() {
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+            <p>Welcome to React development.</p>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
